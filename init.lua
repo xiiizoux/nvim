a0bfb80412ddae9e00324d93490ba18b755b0730
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2025-04-28 13:29:00 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,9 +3,9 @@ require("config.lazy")
 
 -- 通过conda设置python环境
 if vim.loop.os_uname().sysname == "Darwin" or vim.loop.os_uname().sysname == "Linux" then
-  vim.g.python_host_prog = "$HOME/anaconda3/envs/nvim/bin/python"
-  vim.g.python3_host_prog = "$HOME/anaconda3/envs/nvim/bin/python"
+  vim.g.python_host_prog = "$HOME/.uv/nvim/bin/python"
+  vim.g.python3_host_prog = "$HOME/.uv/nvim/bin/python"
 elseif vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.g.python_host_prog = "%USERPROFILE%\\anaconda3\\envs\\nvim\\bin\\python"
-  vim.g.python3_host_prog = "%USERPROFILE%\\anaconda3\\envs\\nvim\\bin\\python"
+  vim.g.python_host_prog = "%USERPROFILE%\\.uv\\nvim\\bin\\python"
+  vim.g.python3_host_prog = "%USERPROFILE%\\.uv\\nvim\\bin\\python"
 end
